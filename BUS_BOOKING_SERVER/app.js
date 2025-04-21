a7c9cc0dedc660984c8cb3a8d17adca68f8bd583
ishansavaliya <ishansavaliya787@gmail.com>
2025-04-21 21:44:12 +0530

    Implement initial server setup with Express, CORS, and MongoDB connection; add Bus, Ticket, and User models

---
+++ BUS_BOOKING_SERVER/app.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/connect.js";
+import { PORT } from "./config/config.js";
+
+dotenv.config();
+
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(PORT, "0.0.0.0", (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Server Started on http://localhost:${PORT}`);
+      }
+    });
+  } catch (error) {
+    console.log("Error in starting server: " + error);
+  }
+};
+
+start();
