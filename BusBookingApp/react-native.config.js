1f0053ef5e7e1974b6a6c500e84251f704e5c375
ishansavaliya <ishansavaliya787@gmail.com>
2025-04-23 20:41:54 +0530

    make setup initial setup for frontend.

---
+++ BusBookingApp/react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  assets: ['./src/assets/fonts/'],
+  getTransformModulePath() {
+    return require.resolve('react-native-typescript-transformer');
+  },
+
+  getSourceExts() {
+    return ['ts', 'tsx'];
+  },
+};
